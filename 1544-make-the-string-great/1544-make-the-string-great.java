3ceb5766984cef5ea93d8774c56c542cda19de6d
anil jaiswal <aniljaiswal5353@gmail.com>
2024-04-05 23:17:18 +0530

    Time: 3 ms (61.07%), Space: 42.2 MB (57.36%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.java
+++ 1544-make-the-string-great/1544-make-the-string-great.java
@@ -1,17 +1,43 @@
 class Solution {
     public String makeGood(String s) {
         
-        //using recursion
+        //method :1 using recursion
         
-        for(int i =0; i< s.length()-1;i++){
+        //for(int i =0; i< s.length()-1;i++){
             
-            if(s.charAt(i) + 32 == s.charAt(i+1) || s.charAt(i) == s.charAt(i+1)+ 32) {
+            //if(s.charAt(i) + 32 == s.charAt(i+1) || s.charAt(i) == s.charAt(i+1)+ 32) {
                 
-                return makeGood( s.substring(0,i) + s.substring(i+2));
-            }
+               // return makeGood( s.substring(0,i) + s.substring(i+2));
+           // }
             
+       // }
+       // return s;
+        
+        //method :2 using stack
+        
+        Stack<Character> stack = new Stack();
+        
+        for(char ch : s.toCharArray()){
+            
+            if(!stack.isEmpty() && Math.abs(stack.peek() -ch) == 32){
+                
+                stack.pop();
+                
+            }else{
+                
+                stack.add(ch);
+            }
+        
         }
-        return s;
+          
+            StringBuilder sb = new StringBuilder();
+            
+            while(stack.size()> 0){
+                
+                sb.append(stack.pop());
+            }
+        
+       return  sb.reverse().toString();
         
     }
 }
