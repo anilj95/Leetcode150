462157465cf09b5257b62674262d16b3cf12733a
anil jaiswal <aniljaiswal5353@gmail.com>
2024-04-18 18:36:37 +0530

    Time: 1 ms (64.19%), Space: 41 MB (14.26%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isHappy(int n) {
+
+       // we will use HashSet to check if that no. exist inside or not coz in many cases, if we keep doing power of both chars
+        // it will come some no. which will repeat again, clearly it is not happy no. if sum ==1, it is happy no.
+        
+        HashSet<Integer> hs = new HashSet<>();
+        
+        while(true){
+            
+            int sum =0;
+           // this loop gives total sum of individual char inside int no. 
+            while(n != 0){
+                
+                sum+= Math.pow(n%10,2); // using power fun of Math class % gives 1st char, 2 is power on 1st char.
+                
+                n= n/10; // gives last char
+            }
+            
+            if( sum ==1) return true;
+            
+            n = sum ; // again assiging to num, for calculating power of both no.s
+            
+            if(hs.contains(n)){
+                
+                return false;
+            }
+            hs.add(n); // add if not present inside hashset
+            
+            
+        }
+        
+    }
+}
