35e6c514234d1fc7c34bae37c91d88ebdcbd3bc5
anil jaiswal <aniljaiswal5353@gmail.com>
2024-04-18 18:52:58 +0530

    Time: 27 ms (15.07%), Space: 58.4 MB (5.75%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,27 +1,32 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
         
+        // 1st load all unique no. inside HashMap, then check if dupicate comes, find last index of duplicate and subtract
+        // with present index if <= k retun true else add to map, coz key is same but this time index is updated to search
+        // for further deplicate which met the criteria of<=k.
+        
         HashMap<Integer,Integer> hm = new HashMap<>();
         
-        for(int i =0; i< nums.length;i++){
-            
+        for(int i = 0; i< nums.length;i++){
+             
             if(!hm.containsKey(nums[i])){
                 
                 hm.put(nums[i],i);
             }else{
                 
-              int pastIndex =  hm.get(nums[i]);
+                int pastIndex = hm.get(nums[i]);
                 
                 if(Math.abs(pastIndex - i)<=k){
-                  return true;  
-                } else{
+                    
+                    return true;
+                }else {
                     
                     hm.put(nums[i],i);
                 }
             }
+            
         }
         
         return false;
-        
     }
 }
