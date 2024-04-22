57809d0c74b1a6ab2ef1d6ad0132476a1bd85395
anil jaiswal <aniljaiswal5353@gmail.com>
2024-04-22 08:29:29 +0530

    Time: 1 ms (60.38%), Space: 42.6 MB (43.42%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,15 +1,17 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         
+        //Total time complexity : for two for loops Tc will be multiple of both for-loops TC : O(n* 2^n)
+        // SC is also same: O(n*2^n)
         List<List<Integer>> res = new ArrayList();
         
         res.add(new ArrayList()); // adding first empty set
         
-        for(int num : nums){
+        for(int num : nums){// TC: n
             
             int n = res.size();// calculating size of dynamically changing arraylist.
             
-            for(int i = 0; i<n ;i++){
+            for(int i = 0; i<n ;i++){ //TC: 2^n // for n given no. total subsets possible = 2^n, for n=3, subsets = 2^3 = 8.
                 
                 List<Integer> temp = new ArrayList(res.get(i));// getting the sussets which we have added to res.
                 
