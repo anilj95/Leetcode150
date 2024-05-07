bc4ae678d3bf5e048247dca99580eb672693b113
anil jaiswal <aniljaiswal5353@gmail.com>
2024-05-07 20:17:54 +0530

    Time: 19 ms (56.63%), Space: 94.4 MB (52.39%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -13,10 +13,10 @@ class Solution {
     }
     
     void generteSubset(int start, int n, List<Integer> curr,List<List<Integer>> subsets, int k){
-        
-        if(curr.size() == k){ // this is extra condn for this problem otherwie code is exactly same.
+        // we want to return combinations only of size k, if k=2, we will not return any subset of size 1 or 3, only 2. 
+        if(curr.size() == k){ // this is extra condn for this problem otherwise code is exactly same.
             
-         subsets.add(new ArrayList(curr));// added empty subset: ->[]
+         subsets.add(new ArrayList(curr));// added new subset: ->[1,2], only when above condn met.
             return; // we dont need to add other size subsets, so we will return
 
         }
@@ -24,8 +24,8 @@ class Solution {
         for(int i = start; i<=n;i++){
             
             curr.add(i);
-            generteSubset(i+1,n, curr, subsets,k); // for next element i+1
-            curr.remove(curr.size()-1); // remove last element for backtracking
+            generteSubset(i+1,n, curr, subsets,k); // for next element i+1, this method will penitrate upto last element->[1,4]
+            curr.remove(curr.size()-1); // then remove last element to come back to origin for next element: backtracking
             
         }
         
